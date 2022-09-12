b1e5a434a50ac5155349dbf34d3243df4605d64c
LennyLucena <asnija426@gmail.com>
2022-09-12 20:55:10 -0300

    Actualizando Navbar

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import {Nav,Carousel} from "react-bootstrap"
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
      {/* Aqui vão os componentes que for criando */}
      
-
-
- </div>
+     <Navbar/>
+    </div>
   );
 }
 
