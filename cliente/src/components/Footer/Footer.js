e1a28eb374a4704f27bcdc898465b28eb4c47b69
Samil Moret <samil.moret@gmail.com>
2022-09-13 15:39:07 -0300

    corrigindo erro de pasta Footer

--- cliente/src/components/Footer/Footer.js
+++ cliente/src/components/Footer/Footer.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { Comentarios } from "./CaixaComentario/CaixaComentarios";
 import './Footer.css';
 
 function Footer() {
     return(
         <div className="footer">
           {/* Aqui van los Componentes CaixaComentario e RedesSocias */}
+          <Comentarios/>
         </div>
     )
 }
