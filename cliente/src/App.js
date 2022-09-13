337b7b2706fd82d454a94eda5f88693e9ccefb11
Pedro Bonett <gilbonett@gmail.com>
2022-09-13 18:32:20 -0300

    adicionando componente no App

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -1,11 +1,8 @@
 import './App.css';
 import IndividualIntervalsHeroImage from './components/HeroImage/HeroImage';
 import Navbar from "./components/Navbar/Navbar";
-import Menu from './components/Menu/Menu';
-import items1 from './Items1';
-import items2 from './Items2';
-import items3 from './Items3';
 import Footer from './components/Footer/Footer';
+import Products from './components/Products/Menu';
 
 
 function App() {
@@ -14,11 +11,7 @@ function App() {
        <>
           <Navbar/>
           <IndividualIntervalsHeroImage/>
-          <div>
-          <Menu name="Churros Simples" desc="Churros saborosos feitos com massas fresquinhas" menu={items1} />
-          <Menu name="Bebidas"  menu={items2} />
-          <Menu name="Combo"  menu={items3} />
-          </div>
+          <Products/>
           <Footer/>
       </>
     </div>
