4d09127143f06cf6a707a009d6c72aa69eb8322c
Pedro Bonett <gilbonett@gmail.com>
2022-09-12 23:54:54 -0300

    adicionando controllers order e product

--- servidor/index.js
+++ servidor/index.js
@@ -7,6 +7,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
+require('./src/controllers/index')(app)
+
 mongoose
   .connect('mongodb+srv://bonett:toti16@cluster0.nf0wacg.mongodb.net/test')
   .then(() => {
