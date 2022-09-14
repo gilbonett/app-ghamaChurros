c0f9c0a0bcde1124b0630bab17736f61750a5bfd
Pedro Bonett <gilbonett@gmail.com>
2022-09-13 22:03:42 -0300

    criando componente home para renderizar components

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -1,21 +1,16 @@
 import './App.css';
-import IndividualIntervalsHeroImage from './components/HeroImage/HeroImage';
-import Navbar from "./components/Navbar/Navbar";
-import Footer from './components/Footer/Footer';
-import Products from './components/Products/Menu';
+import { CartProvider } from './context/cartContext';
+import Home from './components/Home/Home';
 
 
 function App() {
   return (
     <div className="App">
-       <>
-          <Navbar/>
-          <IndividualIntervalsHeroImage/>
-          <Products/>
-          <Footer/>
-      </>
+      <CartProvider>
+          <Home/>
+      </CartProvider>
     </div>
 
       );
-  }
+  };
 export default App; 
