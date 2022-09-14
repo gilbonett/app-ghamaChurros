5bc01129ed8304487f07b9fb0ad9f509d7b3a36e
Pedro Bonett <gilbonett@gmail.com>
2022-09-13 22:01:39 -0300

    usando context para adicionar produtos ao carrinho

--- cliente/src/components/Products/Menu.js
+++ cliente/src/components/Products/Menu.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, {useContext} from "react";
+import CartContext from "../../context/cartContext";
 import { ProductsData } from "../Data";
 import "./Menu.css";
 
 
 
 const Products = () => {
- 
+  /* Traemos del context la funcion para agregar un producto */
+  const { AddItemToCart } = useContext(CartContext);
 
   return (
     <div className="productsMenu">
@@ -21,7 +23,7 @@ const Products = () => {
               </section>
               <div className="btn-price">
               <h6 className="price">${price}</h6>
-              <button className="btn btn-success">Add to Cart</button>
+              <button className="btn btn-success" onClick={() => AddItemToCart(product)}>Add to Cart</button>
               </div>
             </div>
           </article>
