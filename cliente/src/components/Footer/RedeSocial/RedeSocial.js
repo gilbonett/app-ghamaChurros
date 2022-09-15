b8127ca063908bb3cc532d5b26f23fcf28876dca
Samil Moret <samil.moret@gmail.com>
2022-09-15 20:14:34 -0300

    Realizando alterações do footer (red social)

--- cliente/src/components/Footer/RedeSocial/RedeSocial.js
+++ cliente/src/components/Footer/RedeSocial/RedeSocial.js
@@ -9,13 +9,13 @@ function RedeSocial () {
     <p>Preencha o formulário ao lado e entrarei em contato o mais rápido possível.</p>
     <p>Você também podenos  encontrar nas seguintes redes sociais:</p>
 
-     <a href=" " className="Logo-red">
+     <a href=" " className="Logo-red-1">
        <img src="/assets/Face.webp" width="58" height="58"/></a>
 
-     <a href="https://www.instagram.com/moretsamil" className="Logo-red" >
+     <a href="https://www.instagram.com/moretsamil" className="Logo-red-2" >
       <img src="/assets/Instagram.png" width="58" height="58"/></a>
 
-     <a href="https://linkwhats.app/f27e11" className="Logo-red">
+     <a href="https://linkwhats.app/f27e11" className="Logo-red-3">
       <img src="/assets/WhatsApp.png" width="58" height="58"/></a>
   </div>
 </article>
