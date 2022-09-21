9a29622163be58dd767a5ae9757de819de1c624a
Samil Moret <samil.moret@gmail.com>
2022-09-21 20:47:12 -0300

    atualizando rotas de comentarios

---
+++ cliente/src/components/Comentario/Comentario.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+import ListaComentarios from "./ListaComentarios";
+
+
+const Comentario = () => {
+  return (
+    <>
+    <Navbar/>
+    <ListaComentarios/>
+    </>
+    
+  );
+};
+
+export default Comentario;
