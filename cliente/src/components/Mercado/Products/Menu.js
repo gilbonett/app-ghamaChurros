f30e2ddb79a26e2165d291d76f3b2fefaea04835
Pedro Bonett <gilbonett@gmail.com>
2022-09-22 02:26:59 -0300

    consumiento dados de cardapio

--- cliente/src/components/Mercado/Products/Menu.js
+++ cliente/src/components/Mercado/Products/Menu.js
@@ -1,44 +1,58 @@
+import axios from "axios";
 import React, {useContext, useEffect, useState} from "react";
 import CartContext from "../../../context/cartContext";
 //import { ProductsData } from "../Data";
 import "./Menu.css";
 
 
-
-const Products = () => {
+function Products  (){
   /* Traemos del context la funcion para agregar un producto */
   const { AddItemToCart } = useContext(CartContext);
 
-  
+const URL = "http://localhost:8000/products/todos"
 const [item, setItem] = useState([])
 
+useEffect(() => {
+  axios.get(URL).then(res =>{
+    console.log(res.data.products)
+  }).catch(err => {
+    console.log (err)
+  })
+}, []);
 
-function readProducts() {
-fetch ('http://localhost:3000/item/')
-.then ((res) => res.json () )
-.then (data => setItem (data) );
-console.log()
-};
 
- useEffect(() => {
-  readProducts();
- }, []);
+
+//Mapear list de usuarios
+
+
+
+/* const getData = async () => {
+  const response = await axios.get(URL);
+  return console.log(response);
+}
+
+
+useEffect(() => {
+  getData().then((response) => {
+      setItem(response.data);
+  })
+},[setItem])
+  */
 
   return (
     <div className="productsMenu">
-      {item.map((product) => {
-        const { id, title, descricao, price, img } = product;
+      {item.map((products , _id) => {
         return (
-          <article key={id} className="products">
+          <article key={_id} className="products">
             <div className="product-info">
-            <div className="imgCard"><img src={img} alt={title} /></div>
+            <div className="imgCard"><img src={products.img} alt={products.name} /></div>
               <section className="title-descricao">
-                <h2 className="title">{title}</h2>
-                <h6 className="product-text">{descricao}</h6>
+                <h2 className="title">{products.name}</h2>
+                <h6 className="product-text">{products.description}</h6>
               </section>
               <div className="btn-price">
-              <h6 className="price">${price}</h6>
-              <button className="btn btn-success adicionar" onClick={() => AddItemToCart(product)}>Adicionar</button>
+              <h6 className="price">${products.price}</h6>
+              <button className="btn btn-success adicionar" onClick={() => AddItemToCart(products)}>Adicionar</button>
               </div>
             </div>
           </article>
